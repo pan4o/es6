47e7fa58d7d865becec8e2745f46400222931d58
Pancho <panchenko923@gmail.com>
2017-10-27 01:40:18 +0300

    продолжение промисов

--- src/promises.js
+++ src/promises.js
@@ -1,16 +1,51 @@
 
-function applyForVisa(documents, resolve, reject) {
+function applyForVisa(documents) {
 	console.log('Обрботка заявления...');
-	setTimeout(function () {
-		let visa = {};
-		Math.random() > 0.5 ? resolve() : reject('В визе отказанно, не хватило документов');
-	}, 2000)
+
+	var promise = new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			Math.random() > 0.5 ? resolve('visa!!') : reject('В визе отказанно, не хватило документов');
+		}, 2000);
+	});
+
+	return promise;
+
 }
 
-applyForVisa({}, function(visa) {
+function getVisa(visa) {
 	console.log('Виза получена');
-}, function (error) {
-	console.error(error)
-});
+	return visa;
+}
+
+function bookHotel(visa) {
+	console.log(visa)
+	console.log('Отель забронирован');
+	return visa;
+}
+
+function buyTickets (visa) {
+	console.log('Билеты куплены');
+	console.log(visa + ' 2')
+}
+
+function fly() {
+	console.log('Улетели');
+	return new Promise(function (resolve, reject) {
+		//reject('Самолет не взлетел'); ДАЛЬШЕ ВНИЗ НЕ ПОЙДЕТ
+		resolve(); //пойдет дальше
+	});
+}
+
+function land() {
+	console.log('Приземлились');
+}
+
+applyForVisa()
+	.then(getVisa)
+	.then(bookHotel)
+	.then(buyTickets)
+	.then(fly)
+	.then(land)
+	.catch(error => console.log(error));
 
 //https://youtu.be/SjNmkeUpQAU?t=4m28s
